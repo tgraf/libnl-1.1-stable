thom311/libnl

Fix leak of cb if nl_socket_alloc_cb() failed to allocate socket
- each *_get() should have corresponding *_put(). That rule was broken in nl_socket_alloc()
- Also, check if cb is NULL in nl_socket_set_cb (calls BUG())

lib/socket.c
@@ -120,7 +120,7 @@ static struct nl_sock *__alloc_socket(struct nl_cb *cb)
 		return NULL;
 
 	sk->s_fd = -1;
-	sk->s_cb = cb;
+	sk->s_cb = nl_cb_get(cb);
 	sk->s_local.nl_family = AF_NETLINK;
 	sk->s_peer.nl_family = AF_NETLINK;
 	sk->s_seq_expect = sk->s_seq_next = time(0);
@@ -141,12 +141,18 @@ static struct nl_sock *__alloc_socket(struct nl_cb *cb)
 struct nl_sock *nl_socket_alloc(void)
 {
 	struct nl_cb *cb;
-	
+        struct nl_sock *sk;
+
 	cb = nl_cb_alloc(default_cb);
 	if (!cb)
 		return NULL;
 
-	return __alloc_socket(cb);
+        /* will increment cb reference count on success */
+	sk = __alloc_socket(cb);
+
+        nl_cb_put(cb);
+
+        return sk;
 }
 
 /**
@@ -163,7 +169,7 @@ struct nl_sock *nl_socket_alloc_cb(struct nl_cb *cb)
 	if (cb == NULL)
 		BUG();
 
-	return __alloc_socket(nl_cb_get(cb));
+	return __alloc_socket(cb);
 }
 
 /**
@@ -519,6 +525,9 @@ struct nl_cb *nl_socket_get_cb(const struct nl_sock *sk)
 
 void nl_socket_set_cb(struct nl_sock *sk, struct nl_cb *cb)
 {
+        if (cb == NULL)
+                BUG();
+
 	nl_cb_put(sk->s_cb);
 	sk->s_cb = nl_cb_get(cb);
 }

tgraf/libnl-1.1-stable

(cherry picked from commit 33396faca5d48a0318cdd85b4da2c3b247063781)
Conflicts:
	lib/socket.c

lib/socket.c
@@ -184,7 +184,7 @@ static struct nl_handle *__alloc_handle(struct nl_cb *cb)
 	}
 
 	handle->h_fd = -1;
-	handle->h_cb = cb;
+	handle->h_cb = nl_cb_get(cb);
 	handle->h_local.nl_family = AF_NETLINK;
 	handle->h_peer.nl_family = AF_NETLINK;
 	handle->h_seq_expect = handle->h_seq_next = time(0);
@@ -206,14 +206,20 @@ static struct nl_handle *__alloc_handle(struct nl_cb *cb)
 struct nl_handle *nl_handle_alloc(void)
 {
 	struct nl_cb *cb;
-	
+	struct nl_handle *sk;
+
 	cb = nl_cb_alloc(default_cb);
 	if (!cb) {
 		nl_errno(ENOMEM);
 		return NULL;
 	}
 
-	return __alloc_handle(cb);
+	/* will increment cb reference count on success */
+	sk = __alloc_handle(cb);
+
+	nl_cb_put(cb);
+
+	return sk;
 }
 
 /**
@@ -230,7 +236,7 @@ struct nl_handle *nl_handle_alloc_cb(struct nl_cb *cb)
 	if (cb == NULL)
 		BUG();
 
-	return __alloc_handle(nl_cb_get(cb));
+	return __alloc_handle(cb);
 }
 
 /**
@@ -488,6 +494,9 @@ struct nl_cb *nl_socket_get_cb(struct nl_handle *handle)
 
 void nl_socket_set_cb(struct nl_handle *handle, struct nl_cb *cb)
 {
+	if (cb == NULL)
+		BUG();
+
 	nl_cb_put(handle->h_cb);
 	handle->h_cb = nl_cb_get(cb);
 }